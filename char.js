0509c8899a3741159159e82a3cf3c9668eb977a1
subhahu123 <jainsubhahu.gmail.com>
2019-08-13 15:03:30 +0530

    jbj

--- char.js
+++ char.js
@@ -1,9 +1,7 @@
 var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
 
-    // The data for our dataset
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
     data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
@@ -13,7 +11,5 @@ var chart = new Chart(ctx, {
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
-
-    // Configuration options go here
-    options: {}
+    options: options
 });
